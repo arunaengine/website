13956b75625496cfbdf33bf4941335661b87d64f
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-12-01 13:19:41 +0100

    chore: Add Aruna v3 draft API definition

---
+++ types/aruna-v3-enums.ts
@@ -0,0 +1,69 @@
+export enum ComponentType {
+    Server = "Server",
+    Data = "Data",
+    Compute = "Compute"
+}
+export enum Direction {
+    Incoming = "Incoming",
+    Outgoing = "Outgoing",
+    All = "All"
+}
+export enum GenericNodeType {
+    Resource = "Resource"
+}
+export enum GenericNodeType {
+    User = "User"
+}
+export enum GenericNodeType {
+    ServiceAccount = "ServiceAccount"
+}
+export enum GenericNodeType {
+    Group = "Group"
+}
+export enum GenericNodeType {
+    Realm = "Realm"
+}
+export enum GenericNodeType {
+    Component = "Component"
+}
+export enum GetGroupsFromUserResponseGroups {
+    None = "None",
+    Read = "Read",
+    Append = "Append",
+    Write = "Write",
+    Admin = "Admin"
+}
+export enum HashAlgorithm {
+    Sha256 = "Sha256",
+    MD5 = "MD5"
+}
+export enum Permission {
+    None = "None",
+    Read = "Read",
+    Append = "Append",
+    Write = "Write",
+    Admin = "Admin"
+}
+export enum ResourceVariant {
+    Project = "Project",
+    Folder = "Folder",
+    Object = "Object"
+}
+export enum Scope {
+    Personal = "Personal"
+}
+export enum SyncingStatus {
+    Pending = "Pending",
+    Running = "Running",
+    Finished = "Finished",
+    Error = "Error"
+}
+export enum TokenType {
+    Aruna = "Aruna",
+    S3 = "S3"
+}
+export enum VisibilityClass {
+    Public = "Public",
+    PublicMetadata = "PublicMetadata",
+    Private = "Private"
+}
