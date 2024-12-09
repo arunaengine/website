c46780cad62bb14adc3785eb461701f94cfbbce5
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-12-09 16:14:57 +0100

    feat: Add a component for potential analytics content

---
+++ components/custom-ui/dashboard/DummyAnalytics.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="m-4 p-4 rounded-sm border border-dashed border-aruna-text-accent flex items-center justify-center">
+    This is some dummy content for a potential analytics subcomponent.
+  </div>
+</template>
