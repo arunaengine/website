c07bee3701a389502ee46fa1a9cc45ab5df5bacb
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-12-11 16:19:00 +0100

    chore: Replace static string with GenericNodeType enum

--- composables/Projects.ts
+++ composables/Projects.ts
@@ -1,5 +1,6 @@
 import EventBus from "~/composables/EventBus";
 import type {ResourceElement, SearchResponse} from "~/composables/api_wrapper";
+import {GenericNodeType} from "~/types/aruna-v3-enums";
 
 // by convention, composable function names start with "use"
 export async function useProjects() {
@@ -15,7 +16,7 @@ export async function useProjects() {
       if (response) {
         let resources: ResourceElement[] = []
         for (const resource of response._data.resources) {
-          if (resource.type === 'Resource') {
+          if (resource.type === GenericNodeType.Resource) {
             let extended = resource as any // lol, cast to any to allow dynamic adding of properties
             extended.children = []
             resources.push(extended as ResourceElement)
