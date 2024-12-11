f9078142481228d87b4c80ee95e86e6fc82050f9
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-12-11 13:20:14 +0100

    chore: Update aruna API definitions to latest version

--- types/aruna-v3-enums.ts
+++ types/aruna-v3-enums.ts
@@ -1,69 +1,84 @@
 export enum ComponentType {
-    Server = "Server",
-    Data = "Data",
-    Compute = "Compute"
+  Server = "Server",
+  Data = "Data",
+  Compute = "Compute"
 }
+
 export enum Direction {
-    Incoming = "Incoming",
-    Outgoing = "Outgoing",
-    All = "All"
+  Incoming = "Incoming",
+  Outgoing = "Outgoing",
+  All = "All"
 }
+
 export enum GenericNodeType {
-    Resource = "Resource"
+  Resource = "Resource"
 }
+
 export enum GenericNodeType {
-    User = "User"
+  User = "User"
 }
+
 export enum GenericNodeType {
-    ServiceAccount = "ServiceAccount"
+  ServiceAccount = "ServiceAccount"
 }
+
 export enum GenericNodeType {
-    Group = "Group"
+  Group = "Group"
 }
+
 export enum GenericNodeType {
-    Realm = "Realm"
+  Realm = "Realm"
 }
+
 export enum GenericNodeType {
-    Component = "Component"
+  Component = "Component"
 }
+
 export enum GetGroupsFromUserResponseGroups {
-    None = "None",
-    Read = "Read",
-    Append = "Append",
-    Write = "Write",
-    Admin = "Admin"
+  None = "None",
+  Read = "Read",
+  Append = "Append",
+  Write = "Write",
+  Admin = "Admin"
 }
+
 export enum HashAlgorithm {
-    Sha256 = "Sha256",
-    MD5 = "MD5"
+  Sha256 = "Sha256",
+  MD5 = "MD5"
 }
+
 export enum Permission {
-    None = "None",
-    Read = "Read",
-    Append = "Append",
-    Write = "Write",
-    Admin = "Admin"
+  None = "None",
+  Read = "Read",
+  Append = "Append",
+  Write = "Write",
+  Admin = "Admin"
 }
+
 export enum ResourceVariant {
-    Project = "Project",
-    Folder = "Folder",
-    Object = "Object"
+  Project = "Project",
+  Folder = "Folder",
+  Object = "Object"
 }
+
 export enum Scope {
-    Personal = "Personal"
+  Personal = "Personal"
 }
+
 export enum SyncingStatus {
-    Pending = "Pending",
-    Running = "Running",
-    Finished = "Finished",
-    Error = "Error"
+  Pending = "Pending",
+  Running = "Running",
+  Finished = "Finished",
+  Error = "Error"
 }
+
 export enum TokenType {
-    Aruna = "Aruna",
-    S3 = "S3"
+  Aruna = "Aruna",
+  S3 = "S3"
 }
+
 export enum VisibilityClass {
-    Public = "Public",
-    PublicMetadata = "PublicMetadata",
-    Private = "Private"
+  Public = "Public",
+  PublicMetadata = "PublicMetadata",
+  Private = "Private"
 }
