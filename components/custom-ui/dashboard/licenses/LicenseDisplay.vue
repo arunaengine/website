6a4fe635c3d44ae0f0efc7facaebd40258838ff6
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-01-24 15:51:07 +0100

    feat: Add License service functionality to dashboard

---
+++ components/custom-ui/dashboard/licenses/LicenseDisplay.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {ScrollArea} from "~/components/ui/scroll-area";
+
+// ----- Properties ---------- //
+const selectedLicense = defineModel<License>('selectedLicense', {required: false})
+// ----- End Properties ---------- //
+</script>
+<template>
+  <div v-if="selectedLicense"
+       class="p-4 flex flex-col gap-y-4 h-full">
+    <h2 class="mt-6 mb-2 text-2xl font-bold text-aruna-text-accent">
+      {{ selectedLicense.name }}
+    </h2>
+    <Separator/>
+    <div class="font-bold text-aruna-text">
+      {{ selectedLicense.description }}
+    </div>
+    <Separator/>
+    <ScrollArea class="p-4 h-full overflow-hidden">
+        <pre>{{ selectedLicense.terms }}</pre>
+    </ScrollArea>
+  </div>
+</template>
