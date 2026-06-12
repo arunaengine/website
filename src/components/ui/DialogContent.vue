2912c2f6e8e4957b18342c489d6c0a3db06e6a9a
Sebastian Beyvers <sb@pus.de>
2026-06-12 20:29:15 +0200

    fix: wire the dialog close button to actually close

--- src/components/ui/DialogContent.vue
+++ src/components/ui/DialogContent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {
+  DialogClose,
   DialogContent,
   DialogOverlay,
   DialogPortal,
@@ -35,13 +36,13 @@ const classes = computed(() =>
     />
     <DialogContent v-bind="forwarded" :class="classes">
       <slot />
-      <button
+      <DialogClose
         v-if="!hideClose"
         class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring"
         aria-label="Close"
       >
         <X class="h-4 w-4" />
-      </button>
+      </DialogClose>
     </DialogContent>
   </DialogPortal>
 </template>
